101ae901ee4a32f9bee141ea532eabee07904bb0
nazeer14 <133495211+nazeer14@users.noreply.github.com>
2024-11-04 21:40:10 +0530

    Update Member.java

--- Member.java
+++ Member.java
@@ -6,13 +6,15 @@ import java.util.HashMap;
 
 public class Member
 {
-	HashMap<Integer ,String> members1=new HashMap<>();
-	HashMap<Integer ,String> members2=new HashMap<>();
+	//take some example names 
 	String member[]={"Prabhas","Powen","Mahesh","Arjun","Srinu","Ntr","Srikanth","atlee","Ramu","Sai","Naresh","Shiva"};
+	//take some random numbers
 	int ids[]={101,102,103,104,105,106,107,108,109,110,111,112};	
 	public void members(String name) 
 	{
-		if(name.equalsIgnoreCase("Admin101")){
+		if(name.equalsIgnoreCase("Admin101"))//Admin101 is just a string you can chage it
+		{
+			//print books available in array
 			System.out.println("Books:"+Arrays.toString(member));
 		}
 		else{
